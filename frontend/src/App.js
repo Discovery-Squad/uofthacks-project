cb6382008afb12895d273dc0c56b2ec0f1750383
Faraaz <faraazmohsin100@gmail.com>
2023-01-21 14:59:54 -0500

    react router setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,41 @@
-//import logo from './logo.svg';
-//import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HomeScreen } from './containers/homescreen/homescreen';
+import { Explore } from './containers/map/explore';
+import { Intro } from './containers/onboarding/intro';
+import { Login } from './containers/onboarding/login';
+import { SignUp } from './containers/onboarding/signup';
+import { Preferences } from './containers/preferences/preferences';
+import { MyProfile } from './containers/profile/myprofile';
+import { ThemedRun } from './containers/themedrun/run';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      
+      //Onboard
+      <Route exact path="/" component={Intro}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/signup" component={SignUp}/>
+
+      //Homescreen
+      <Route path="/home" component={HomeScreen}/>
+
+      //Map
+      <Route path="/explore" component={Explore}/>
+
+      //Run
+      <Route path="/run" component={ThemedRun}/>
+
+      //Preferences
+      <Route path="/preferences" component={Preferences}/>
+
+      //Profile
+      <Route path="/myprofile" component={MyProfile}/>
+
+    </Switch>
+    </BrowserRouter>
   );
 }
 
